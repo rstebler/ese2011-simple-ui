96ef4be7c8a2a2a019b77b99460cd68576aec1f7
Roger Stebler <roger.stebler@students.ethz.ch>
2011-10-04 20:23:16 +0200

    updated view

--- app/models/Bootstrap.java
+++ app/models/Bootstrap.java
@@ -19,18 +19,21 @@ public class Bootstrap extends Job {
     	event=new Event("Sepp's Event 2", now, now, false, user);
     	user.getCalendarByName(user.getName()+"'s Calendar").addEvent(event, user);
     	Database.addUser(user);
+    	
     	user = new User("Fritz", "123");
     	event=new Event("Fritz's Event 1", now, now, false, user);
     	user.getCalendarByName(user.getName()+"'s Calendar").addEvent(event, user);
     	event=new Event("Fritz's Event 2", now, now, true, user);
     	user.getCalendarByName(user.getName()+"'s Calendar").addEvent(event, user);
     	Database.addUser(user);
+    	
     	user = new User("Hans", "hello");
     	event=new Event("Hans's Event 1", now, now, true, user);
     	user.getCalendarByName(user.getName()+"'s Calendar").addEvent(event, user);
     	event=new Event("Hans's Event 2", now, now, true, user);
     	user.getCalendarByName(user.getName()+"'s Calendar").addEvent(event, user);
     	Database.addUser(user);
+    	
     	user = new User("Kurt", "password");
     	event=new Event("Kurt's Event 1", now, now, false, user);
     	Calendar cal1 = new Calendar("Kurt's Kalender", user);
